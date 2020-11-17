b219aa1a40f0b098c7bd510ab3bdf6eb4c461abc
FelEstrela <festrela@fvet.uba.ar>
2020-11-16 21:12:10 -0300

    modificado messi.js

--- messi.js
+++ messi.js
@@ -1,1 +1,2 @@
-let messi = "Lionel Messi es el mejor jugador de la historia del fútbol"
+let messi = "Lionel Messi es el mejor jugador de la historia del fútbol";
+let messi = "El mejor camisa 10 de los ultimos tiempos";
